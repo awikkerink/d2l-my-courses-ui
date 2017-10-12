3ac3692a859391577d992a45daea6e4be3a59635
Ryan Mahler <ryan.mahler@d2l.com>
2017-10-12 15:25:54 -0400

    US90997 Add new tests for new components
    Also fixed a few small errors in stuff that came up during testing.

--- test/d2l-filter-menu/d2l-filter-menu-tab.js
+++ test/d2l-filter-menu/d2l-filter-menu-tab.js
@@ -3,11 +3,21 @@
 'use strict';
 
 var sandbox,
-	component;
+	component,
+	organization;
 
 beforeEach(function() {
 	sandbox = sinon.sandbox.create();
 	component = fixture('d2l-filter-menu-tab-fixture');
+	component.searchAction = {
+		name: 'search',
+		href: '/'
+	};
+	organization = {
+		class: ['active', 'semester'],
+		rel: ['https://api.brightspace.com/rels/organization'],
+		href: '/organizations/1'
+	};
 });
 
 afterEach(function() {
@@ -15,7 +25,114 @@ afterEach(function() {
 });
 
 describe('d2l-filter-menu-tab', function() {
-	it('should exist', function() {
-		expect(component).to.exist;
+	describe('DOM manipulation', function() {
+		it('should show empty state message initially', function() {
+			expect(component.$$('.no-items-text').getAttribute('hidden')).to.be.null;
+		});
+
+		it('should show content once the tab has loaded', function() {
+			component.fetchSirenEntity = sandbox.stub().returns(Promise.resolve({
+				entities: [organization]
+			}));
+
+			return component.load().then(function() {
+				expect(component.$$('.no-items-text').getAttribute('hidden')).to.not.be.null;
+			});
+		});
+
+		it('should show the No Results message when there are no search results', function() {
+			component.fire('d2l-search-widget-results-changed', {
+				entities: []
+			});
+
+			expect(component._allFilters.length).to.equal(0);
+			expect(component._hasSearchResults).to.be.false;
+			expect(component.$$('div > div.no-items-text').getAttribute('hidden')).to.be.null;
+		});
+
+		it('should not show the No Results message when there are search results', function() {
+			component.fire('d2l-search-widget-results-changed', {
+				entities: [organization]
+			});
+
+			expect(component._allFilters.length).to.equal(1);
+			expect(component._hasSearchResults).to.be.true;
+			expect(component.$$('div > div.no-items-text').getAttribute('hidden')).to.not.be.null;
+		});
+	});
+
+	describe('selectedFilters', function() {
+		it('should generate an event when selectedFilters is changed', function(done) {
+			var listener = function() {
+				component.removeEventListener('selected-filters-changed', listener);
+				done();
+			};
+			component.addEventListener('selected-filters-changed', listener);
+
+			component.selectedFilters = [organization];
+		});
+	});
+
+	describe('when a d2l-menu-item-change event occurs', function() {
+		it('should add the filter if it was selected', function(done) {
+			component._allFilters = [organization];
+			component.selectedFilters = [];
+
+			var listener = function() {
+				component.removeEventListener('d2l-menu-item-change', listener);
+				expect(component.selectedFilters.length).to.equal(1);
+
+				done();
+			};
+			component.addEventListener('d2l-menu-item-change', listener);
+
+			component.fire('d2l-menu-item-change', {
+				selected: true,
+				value: organization
+			});
+		});
+
+		it('should remove the filter if it was deselected', function(done) {
+			component._allFilters = [organization];
+			component.selectedFilters = [organization];
+
+			var listener = function() {
+				component.removeEventListener('d2l-menu-item-change', listener);
+				expect(component.selectedFilters.length).to.equal(0);
+
+				done();
+			};
+			component.addEventListener('d2l-menu-item-change', listener);
+
+			component.fire('d2l-menu-item-change', {
+				selected: false,
+				value: organization
+			});
+		});
+
+		it('should fire a selected-filters-changed event', function(done) {
+			var listener = function() {
+				component.removeEventListener('selected-filters-changed', listener);
+				done();
+			};
+			component.addEventListener('selected-filters-changed', listener);
+
+			component.fire('d2l-menu-item-change', {
+				selected: false,
+				value: organization
+			});
+		});
+	});
+
+	describe('_checkSelected', function() {
+		it('should return true if item should be selected', function() {
+			component.selectedFilters = [organization.href];
+			expect(component._checkSelected(organization)).to.be.true;
+		});
+
+		it('should return false if the item should not be selected', function() {
+			component.selectedFilters = ['foo'];
+			expect(component._checkSelected(organization)).to.be.false;
+		});
 	});
 });
--- test/d2l-filter-menu/d2l-filter-menu.js
+++ test/d2l-filter-menu/d2l-filter-menu.js
@@ -3,9 +3,19 @@
 'use strict';
 
 var sandbox,
-	component;
+	component,
+	myEnrollmentsEntity;
 
 beforeEach(function() {
+	myEnrollmentsEntity = window.D2L.Hypermedia.Siren.Parse({
+		actions: [{
+			name: 'search-my-semesters',
+			href: '/enrollments'
+		}, {
+			name: 'search-my-departments',
+			href: '/enrollments'
+		}]
+	});
 	sandbox = sinon.sandbox.create();
 	component = fixture('d2l-filter-menu-fixture');
 });
@@ -15,7 +25,131 @@ afterEach(function() {
 });
 
 describe('d2l-filter-menu', function() {
-	it('should exist', function() {
-		expect(component).to.exist;
+	it('should update actions from myEnrollmentsEntity', function() {
+		var spy = sandbox.spy(component, '_myEnrollmentsEntityChanged');
+
+		component.myEnrollmentsEntity = myEnrollmentsEntity;
+
+		expect(spy.called).to.be.true;
+		expect(component._searchDepartmentsAction.name).to.equal('search-my-departments');
+		expect(component._searchDepartmentsAction.href).to.equal('/enrollments');
+		expect(component._searchSemestersAction.name).to.equal('search-my-semesters');
+		expect(component._searchSemestersAction.href).to.equal('/enrollments');
+	});
+
+	describe('setting org unit type names', function() {
+		it('should set the noFilterText correctly', function() {
+			component.filterStandardSemesterName = 'foo';
+			component.filterStandardDepartmentName = 'bar';
+
+			var expectedText = 'You do not currently have any filter options as you are not enrolled in a foo or bar.';
+			expect(component._noFiltersText).to.equal(expectedText);
+		});
+
+		describe('tab text', function() {
+			it('should render semester name', function() {
+				component.filterStandardSemesterName = 'foo';
+
+				var button = component.$.semestersTabButton;
+				expect(button.innerText).to.equal('foo');
+			});
+
+			it('should render semester name with 1 filter', function() {
+				component.filterStandardSemesterName = 'foo';
+				component.semesterFilters = [1];
+
+				var button = component.$.semestersTabButton;
+				expect(button.innerText).to.equal('foo (1)');
+			});
+
+			it('should render department name', function() {
+				component.filterStandardDepartmentName = 'foo';
+
+				var button = component.$.departmentsTabButton;
+				expect(button.innerText).to.equal('foo');
+			});
+
+			it('should render department name with 1 filter', function() {
+				component.filterStandardDepartmentName = 'foo';
+				component.departmentFilters = [1];
+
+				var button = component.$.departmentsTabButton;
+				expect(button.innerText).to.equal('foo (1)');
+			});
+		});
+
+		describe('search input placeholder text', function() {
+			it('should set the search placeholder text for semesters', function() {
+				component.filterStandardSemesterName = 'foo';
+
+				expect(component._semestersSearchPlaceholderText).to.equal('Search by foo');
+			});
+
+			it('should set the search placeholder text for departments', function() {
+				component.filterStandardDepartmentName = 'foo';
+
+				expect(component._departmentsSearchPlaceholderText).to.equal('Search by foo');
+			});
+		});
+	});
+
+	describe('clear button', function() {
+		beforeEach(function() {
+			component.semesterFilters = [];
+			component.departmentFilters = [];
+		});
+
+		it('should be hidden when there are no filters selected', function() {
+			expect(component.$$('.clear-button').getAttribute('hidden')).to.not.be.null;
+		});
+
+		it('should appear when at least one semester filter is selected', function() {
+			component.semesterFilters = [1];
+			component.fire('selected-filters-changed');
+
+			expect(component.$$('.clear-button').getAttribute('hidden')).to.be.null;
+		});
+
+		it('should appear when at least one department filter is selected', function() {
+			component.departmentFilters = [1];
+			component.fire('selected-filters-changed');
+
+			expect(component.$$('.clear-button').getAttribute('hidden')).to.be.null;
+		});
+
+		it('should clear filters when clicked', function() {
+			component.semesterFilters = [1];
+			component.fire('selected-filters-changed');
+
+			expect(component.$$('.clear-button').getAttribute('hidden')).to.be.null;
+
+			component.$$('.clear-button').click();
+			expect(component.$$('.clear-button').getAttribute('hidden')).to.not.be.null;
+		});
+	});
+
+	describe('currentFilters', function() {
+		it('should update currentFilters when selected-filters-changed is fired', function() {
+			component.semesterFilters = [1];
+			component.departmentFilters = [1];
+			expect(component.currentFilters.length).to.equal(0);
+
+			component.fire('selected-filters-changed');
+			expect(component.currentFilters.length).to.equal(2);
+		});
+
+		it('should fire a d2l-filter-menu-change event when currentFilters changes', function(done) {
+			var listener = function() {
+				expect(component.currentFilters.length).to.equal(2);
+				component.removeEventListener('d2l-filter-menu-change', listener);
+				done();
+			};
+
+			component.addEventListener('d2l-filter-menu-change', listener);
+
+			component.semesterFilters = [1];
+			component.departmentFilters = [1];
+			component.fire('selected-filters-changed');
+		});
 	});
 });
